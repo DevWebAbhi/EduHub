f9e73586d3c89d83c397164286c1301c3e239346
DevWebAbhi <tiwariabhishek889912@gmail.com>
2024-03-10 16:42:44 +0530

    ok

--- backend/Models/courseModel.js
+++ backend/Models/courseModel.js
@@ -2,7 +2,7 @@
 const mongoose=require("mongoose");
 
 const schema=mongoose.Schema({
-    name:{type:String,required:true},
+    title:{type:String,required:true},
     image:{type:String,required:true},
     description:{type:String,required:true},
     checklist:{type:String,required:true},
